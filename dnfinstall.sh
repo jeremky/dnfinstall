ba3f02761c6c28a8973fc8b25c042d356bb1ec70
JeremKy <jeremky@me.com>
2026-08-16 19:31:22 +0200

    Ajout de la gestion des erreurs sur les fonctions
    Désactive aussi PasswordAuthentication par défaut dans la configuration SSH.

--- dnfinstall.sh
+++ dnfinstall.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #!/bin/bash -e
 
 # Messages en couleur
@@ -20,12 +21,11 @@ fi
 # Fonctions
 install_packages() {
   warning "Mise à jour des paquets..."
-  dnf -y upgrade
+  dnf -y upgrade || { error "Problème lors de la mise à jour des paquets"; }
   if [[ -f "$list" ]]; then
     warning "Installation des paquets..."
     grep -v -e '#' -e '^$' "$list" | xargs dnf -y install || {
       error "Problème lors de l'installation des paquets"
-      exit 1
     }
     message "Installation des paquets terminée"
   fi
@@ -33,20 +33,28 @@ install_packages() {
 
 enable_flathub() {
   warning "Activation de Flathub..."
-  flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+  flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo || {
+    error "Problème lors de l'activation de Flathub"
+  }
   message "Flathub activé"
 }
 
 disable_tty1() {
   warning "Désactivation du tty1..."
-  systemctl disable getty@tty1
+  systemctl disable getty@tty1 || {
+    error "Problème lors de la désactivation du tty1"
+  }
   message "tty1 désactivé"
 }
 
 disable_sudopasswd() {
-  warning "Désactivation du mot de pass pour les utilisateurs sudo..."
-  echo "%wheel ALL=(ALL) NOPASSWD: ALL" >/etc/sudoers.d/010_nopasswd
-  chmod 440 /etc/sudoers.d/010_nopasswd
+  warning "Désactivation du mot de passe pour les utilisateurs sudo..."
+  echo "%wheel ALL=(ALL) NOPASSWD: ALL" >/etc/sudoers.d/010_nopasswd || {
+    error "Problème lors de la configuration de sudo"
+  }
+  chmod 440 /etc/sudoers.d/010_nopasswd || {
+    error "Problème lors de la configuration des permissions sudo"
+  }
   message "Mot de passe sudo désactivé"
 }
 
@@ -54,8 +62,12 @@ configure_sshd() {
   if [[ -d /etc/ssh/sshd_config.d ]]; then
     warning "Sécurisation de SSH..."
     user=$(id -un 1000)
-    echo -e "# Secure Config\nX11Forwarding no\nAllowUsers $user\nHostKey /etc/ssh/ssh_host_ed25519_key\nPasswordAuthentication yes\nKbdInteractiveAuthentication yes\nMaxAuthTries 3\nClientAliveInterval 300\nClientAliveCountMax 2\nKexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org\nMACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com\nCiphers aes256-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-gcm@openssh.com,aes128-ctr" >"/etc/ssh/sshd_config.d/$user.conf"
-    systemctl restart sshd
+    echo -e "# Secure Config\nX11Forwarding no\nAllowUsers $user\nHostKey /etc/ssh/ssh_host_ed25519_key\nPasswordAuthentication no\nKbdInteractiveAuthentication yes\nMaxAuthTries 3\nClientAliveInterval 300\nClientAliveCountMax 2\nKexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org\nMACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com\nCiphers aes256-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-gcm@openssh.com,aes128-ctr" >"/etc/ssh/sshd_config.d/$user.conf" || {
+      error "Problème lors de la configuration de SSH"
+    }
+    systemctl restart sshd || {
+      error "Problème lors du redémarrage de SSH"
+    }
     message "SSH sécurisé. Modifiez le fichier /etc/ssh/sshd_config.d/$user.conf pour désactiver la connexion par mot de passe après avoir importé votre clé ed25519."
   fi
 }
